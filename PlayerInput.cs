319cd466ded80b9b954b938e3217cd6db0fc4275
Justin <justinjchou@gmail.com>
2022-08-23 23:50:39 -0700

    2D Top Down controls v2.0

---
+++ PlayerInput.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    Rigidbody2D body;
+    float horizontal;
+    float vertical;
+
+    public float runSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+}
