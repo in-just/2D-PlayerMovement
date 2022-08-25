8acd77ce181f4f82860b08e13698072874e350c3
Justin <justinjchou@gmail.com>
2022-08-24 20:40:19 -0700

    Very simple follower pathing

---
+++ GhostPath.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostPath : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    Rigidbody2D rb;
+    Transform target;
+    Transform target2;
+    Vector2 moveDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        target = GameObject.Find("Player").transform;
+    }
+
+    void Update()
+    {
+        if (target)
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            rb.rotation = angle;
+            moveDirection = direction;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (target)
+        {
+            rb.velocity = new Vector2(moveDirection.x, moveDirection.y) * moveSpeed;
+        }
+    }
+}
